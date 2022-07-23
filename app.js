7cf1b8434f9bb2387afbd5fb19b79db328335cb3
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-07-23 02:22:08 +0100

    made more changes

--- app.js
+++ app.js
@@ -8,69 +8,69 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-console.log(date.getDay())
 
 
 
-// // the different tasks lists 
-// let items = ['buy food', 'cook food', 'eat food'];
-// let workItems = []
+// the different tasks lists 
+let items = ['buy food', 'cook food', 'eat food'];
+let workItems = []
+
+// a function from the module i created 
+let day = date.getDate()
 
 
 
+app.get('/', function(req, res){
+  res.render('list', {listTitle: day, listItems: items});
 
-
-// app.get('/', function(req, res){
-//   res.render('list', {listTitle: day, listItems: items});
-
-// })
+})
 
 
 
-// app.post('/', function(req, res){
+app.post('/', function(req, res){
   
-//   var item = req.body.newItem;
+  var item = req.body.newItem;
 
-//   if(req.body.list === "Work"){
-//     workItems.push(item)
-//     res.redirect('/work')
-//   } 
-//   else{
-//     items.push(item)
-//     res.redirect('/')
-//   }
+  if(req.body.list === "Work"){
+    workItems.push(item)
+    res.redirect('/work')
+  } 
+  else{
+    items.push(item)
+    res.redirect('/')
+  }
   
 
-// })
+})
 
 
 
 
-// app.get('/work', function(req, res){
-//   res.render('list', {listTitle: "Work", listItems: workItems})
-// })
+app.get('/work', function(req, res){
+  res.render('list', {listTitle: "Work", listItems: workItems})
+})
 
 
 
-// app.post('/work', function(req, res){
-//   let item = req.body.newItem
-//   workItems.push(item);
-//   res.redirect('/work')
-// })
-
-
-
-
-
+app.post('/work', function(req, res){
+  let item = req.body.newItem
+  workItems.push(item);
+  res.redirect('/work')
+})
 
 
 
 
 
 
-// app.listen(3000, function(){
-//   console.log("LIUHGY")
-// })
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Listening....")
+})
 
 
 
